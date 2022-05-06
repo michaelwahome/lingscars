d6be78517c4db343089a5e3c3489de7f095bd13e
michaelwahome <michaelwahome1@gmail.com>
2022-05-06 07:55:41 +0300

    Added routes and controllers and renamed files. Added Home and Auth route and controller for landing page and login. Renamed "login_view.html" to "login_view.php".

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Auth extends BaseController
 {
     public function index()
     {
-        return view('landing-page');
+        return view('login_view');
     }
 }
