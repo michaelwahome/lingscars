9644da851b57491776ab09124cdeb87e56d5b959
AbigaelKirwa <55990700+AbigaelKirwa@users.noreply.github.com>
2022-06-22 18:42:14 +0300

    made overall catalogue dynamic

--- app/Views/overall_catalogue.php
+++ app/Views/overall_catalogue.php
@@ -8,6 +8,10 @@
  
 ?>
 
+    <!-- attempting to display data from database -->
+
+    <!-- end of displaying from database -->
+
     <!-- this section is the one above the navigation -->
     <section class="top">
 
@@ -27,197 +31,29 @@
             </div>
             <div class="frame-two">
                 <h1><b>Vehicle Highlights</b></h1>
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame6.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Toyota Highlander</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1000</b></p>
-                            <p class="black-words"><b>€800</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
             </div>
-            <div class="frame-three">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Range Rover Prado</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1200</b></p>
-                            <p class="black-words"><b>€900</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame3.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-four">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame4.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Volkswagen Prado</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1500</b></p>
-                            <p class="black-words"><b>€700</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-five">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Toyota RAV4</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€2000</b></p>
-                            <p class="black-words"><b>€1200</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame5.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-two">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame7.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Yamaha YZF-36</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1000</b></p>
-                            <p class="black-words"><b>€800</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-three">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Yamaha Tenere 700</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1200</b></p>
-                            <p class="black-words"><b>€900</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame8.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-four">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame12.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Yamaha star venture</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1500</b></p>
-                            <p class="black-words"><b>€700</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-five">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Yamaha WR250R</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€2000</b></p>
-                            <p class="black-words"><b>€1200</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame11.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-two">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame15.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Toyota Vitz</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1000</b></p>
-                            <p class="black-words"><b>€800</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-three">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Honda Civic</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1200</b></p>
-                            <p class="black-words"><b>€900</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame14.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-four">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame13.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Toyota Crown</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1500</b></p>
-                            <p class="black-words"><b>€700</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-five">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2 class="car_names"><b>Honda Insight</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€2000</b></p>
-                            <p class="black-words"><b>€1200</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame17.png" alt="car">
-                    </div>
-                </div>
-            </div>
+            <table class="table table-hover">
+
+                <?php foreach ($images as $row): ?>
+
+                <tbody>
+                <tr>
+
+                <td class="td-car"><img src="<?php echo $row['image']; ?>" alt="" srcset=""></td>
+                
+                <td class="td-words">
+                    <h2 class="vehicle_name"><b><?php echo $row['vehicle_model']; ?></b></h2>
+                    <p><?php echo $row['vehicle_description']; ?></p>
+                    <b>€<?php echo $row['unit_price']; ?></b>
+                    <input type="button" value="add to cart" class="btn btn-dark">
+                
+                </td>
+
+                </tr>
+                </tbody>
+
+                <?php endforeach; ?> 
+            </table>
         </div>
     </section>
    
