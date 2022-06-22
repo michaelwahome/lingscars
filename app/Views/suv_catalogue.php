d3121fc12899445eef28703d83e8f563fe3f4c93
AbigaelKirwa <55990700+AbigaelKirwa@users.noreply.github.com>
2022-06-22 18:43:24 +0300

    made suv catalogue dynamic

--- app/Views/suv_catalogue.php
+++ app/Views/suv_catalogue.php
@@ -27,68 +27,28 @@
             </div>
             <div class="frame-two">
                 <h1><b>SUV Highlights</b></h1>
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame6.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Toyota Highlander</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1000</b></p>
-                            <p class="black-words"><b>€800</b></p>
-                        </div>
+                <table class="table table-hover">
+
+                    <?php foreach ($images as $row): ?>
+
+                    <tbody>
+                    <tr>
+
+                    <?php if($row['category_id'] == 2): ?>
+                    <td class="td-car"><img src="<?php echo $row['image']; ?>" alt="" srcset=""></td>
+                    <td class="td-words">
+                        <h2 class="vehicle_name"><b><?php echo $row['vehicle_model']; ?></b></h2>
+                        <p><?php echo $row['vehicle_description']; ?></p>
+                        <b>€<?php echo $row['unit_price']; ?></b>
                         <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-three">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Range Rover Prado</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1200</b></p>
-                            <p class="black-words"><b>€900</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame3.png" alt="car">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-four">
-                <div class="image-and-words">
-                    <div class="picture">
-                        <img src="images/frame4.png" alt="">
-                    </div>
-                    <div class="words">
-                        <h2><b>Volkswagen Prado</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€1500</b></p>
-                            <p class="black-words"><b>€700</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                </div>
-            </div>
-            <div class="frame-five">
-                <div class="image-and-words">
-                    <div class="words">
-                        <h2><b>Toyota RAV4</b></h2>
-                        <p>This model is amazing. Features are even better than the last model. You will get maximum mileage from this. Enjoy the sleek new white color that is now available. Reliable engine and amazing wheels that are sure to stand the test of time</p>
-                        <div class="grey-and-black-words">
-                            <p class="grey-words"><b>€2000</b></p>
-                            <p class="black-words"><b>€1200</b></p>
-                        </div>
-                        <input type="button" value="add to cart" class="btn btn-dark">
-                    </div>
-                    <div class="picture">
-                        <img src="images/frame5.png" alt="car">
-                    </div>
-                </div>
+                    </td>
+                    <?php endif ?>
+
+                    </tr>
+                    </tbody>
+
+                    <?php endforeach; ?> 
+                </table>
             </div>
         </div>
     </section>
