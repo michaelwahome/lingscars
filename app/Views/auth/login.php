e55262f6516d7ab51089eb72fa7cfc70cc1c3d52
michaelwahome <michaelwahome1@gmail.com>
2022-06-23 16:01:38 +0300

    Fixed css not showing on failed login.

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce</title>
-    <link rel="stylesheet" href="../css/register.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../css/register.css?v=<?php echo time(); ?>">
     <script src="https://kit.fontawesome.com/cf05e83bf0.js" crossorigin="anonymous"></script>
 </head>
 <body>
