c3b1dd4a34c5f258fbe73d0835014d3afd2fdca1
michaelwahome <michaelwahome1@gmail.com>
2022-06-28 21:16:28 +0300

    Logout from admin side.

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,6 +6,15 @@ class Dashboard extends BaseController
 {
     public function index()
     {
+        $session = session();
+        
+        if(!isset($_SESSION["user_details"]))
+        {
+            return redirect()->to('/');
+        }elseif($_SESSION["user_details"]["role_id"] == 1){
+            return redirect()->to('/');
+        }
+
         return view('admin/dashboard');
     }
 }
